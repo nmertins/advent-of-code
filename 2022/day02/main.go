5d445c1b4d202b2b8e793d782e9bc67cac92b83b
Nathan Mertins <nwmertins@gmail.com>
2022-12-02 22:38:12 -0600

    parser for day02 input

--- 2022/day02/main.go
+++ 2022/day02/main.go
@@ -1,11 +1,37 @@
 package main
 
-type RockPaperScissorsShape string
-
-const (
-	Rock RockPaperScissorsShape = "A"
+import(
+	"strings"
 )
 
-func ParseStrategy(strategy string) {
+type RockPaperScissorsShape int
 
+const (
+	Rock RockPaperScissorsShape = iota
+	Paper
+	Scissors
+)
+
+func ParseInput(strategy string) (RockPaperScissorsShape, RockPaperScissorsShape) {
+	shapes := strings.Split(strategy, " ")
+	return convertStringToShape(shapes[0]), convertStringToShape(shapes[1])
+}
+
+func convertStringToShape(s string) RockPaperScissorsShape {
+	switch s {
+	case "A":
+		return Rock
+	case "X":
+		return Rock
+	case "B":
+		return Paper
+	case "Y":
+		return Paper
+	case "C":
+		return Scissors
+	case "Z":
+		return Scissors
+	default:
+		return -1
+	}
 }
--- 2022/day02/main_test.go
+++ 2022/day02/main_test.go
@@ -9,6 +9,19 @@ import (
 func TestDay02(t *testing.T) {
 	t.Run("parse sample input", func(t *testing.T) {
 		input := utils.ReadFile("resources/sample_input.txt")
-		t.Fail()
+		opponentShape, myShape := ParseInput(input[0])
+		assertShapesEqual(t, Rock, opponentShape)
+		assertShapesEqual(t, Paper, myShape)
+
+		opponentShape, myShape = ParseInput(input[1])
+		assertShapesEqual(t, Paper, opponentShape)
+		assertShapesEqual(t, Rock, myShape)
 	})
+}
+
+func assertShapesEqual(t testing.TB, expected RockPaperScissorsShape, actual RockPaperScissorsShape) {
+	t.Helper()
+	if actual != expected {
+		t.Fatalf("expected %v, got %v", expected, actual)
+	}
 }
