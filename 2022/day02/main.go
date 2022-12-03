a5eab5f86b01921e2f5a243d8acf86e24385e361
Nathan Mertins <nwmertins@gmail.com>
2022-12-03 00:05:52 -0600

    part 2 solution

--- 2022/day02/main.go
+++ 2022/day02/main.go
@@ -8,11 +8,18 @@ import(
 )
 
 type RockPaperScissorsShape int
-
 const (
 	Rock RockPaperScissorsShape = iota + 1
 	Paper
 	Scissors
+)
+
+type GameOutcome int
+const (
+	multiplier = 3
+	Win GameOutcome = iota * multiplier
+	Lose
+	Draw
 )
 
 func (s RockPaperScissorsShape) Compare(other RockPaperScissorsShape) int {
@@ -53,9 +60,55 @@ func (s RockPaperScissorsShape) Compare(other RockPaperScissorsShape) int {
 	return -1
 }
 
+func (s RockPaperScissorsShape) BackoutDesiredShape(desiredOutcome GameOutcome) RockPaperScissorsShape {
+	if s == Rock {
+		if desiredOutcome == Win {
+			return Paper
+		}
+		if desiredOutcome == Lose {
+			return Scissors
+		}
+		if desiredOutcome == Draw {
+			return Rock
+		}
+	}
+	if s == Paper {
+		if desiredOutcome == Win {
+			return Scissors
+		}
+		if desiredOutcome == Lose {
+			return Rock
+		}
+		if desiredOutcome == Draw {
+			return Paper
+		}
+	}
+	if s == Scissors {
+		if desiredOutcome == Win {
+			return Rock
+		}
+		if desiredOutcome == Lose {
+			return Paper
+		}
+		if desiredOutcome == Draw {
+			return Scissors
+		}
+	}
+
+	return -1
+}
+
 func ParseInput(strategy string) (RockPaperScissorsShape, RockPaperScissorsShape) {
 	shapes := strings.Split(strategy, " ")
 	return convertStringToShape(shapes[0]), convertStringToShape(shapes[1])
+}
+
+func ParseInput2(strategy string) (RockPaperScissorsShape, RockPaperScissorsShape) {
+	shapes := strings.Split(strategy, " ")
+	opponentShape := convertStringToShape(shapes[0])
+	gameOutcome := convertStringToShapePart2(shapes[1])
+	myShape := opponentShape.BackoutDesiredShape(gameOutcome)
+	return opponentShape, myShape
 }
 
 func convertStringToShape(s string) RockPaperScissorsShape {
@@ -77,6 +130,19 @@ func convertStringToShape(s string) RockPaperScissorsShape {
 	}
 }
 
+func convertStringToShapePart2(winLoseDraw string) GameOutcome {
+	switch winLoseDraw {
+	case "X":
+		return Lose
+	case "Y":
+		return Draw
+	case "Z":
+		return Win
+	default:
+		return -1
+	}
+}
+
 func Score(opponentShape RockPaperScissorsShape, myShape RockPaperScissorsShape) int {
 	return int(myShape) + myShape.Compare(opponentShape)
 }
@@ -85,7 +151,7 @@ func main() {
 	input := utils.ReadFile("resources/input.txt")
 	total := 0
 	for _, line := range input {
-		opponentShape, myShape := ParseInput(line)
+		opponentShape, myShape := ParseInput2(line)
 		total += Score(opponentShape, myShape)
 	}
 	fmt.Println(total)
