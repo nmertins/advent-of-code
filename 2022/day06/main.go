ca4f7cea5ffb80f8935ba47939e3ab05c546a262
Nathan Mertins <nwmertins@gmail.com>
2022-12-07 07:11:32 -0600

    day 6 part 1

--- 2022/day06/main.go
+++ 2022/day06/main.go
@@ -1,11 +1,18 @@
 package main
 
+import (
+	"fmt"
+	"strings"
+
+	"github.com/nmertins/advent-of-code/2022/utils"
+)
+
 const (
 	StartOfPacketMarkerLength = 4
 )
 
 func FindMarker(input string) string {
-	for i := 0; i < len(input)-4; i++ {
+	for i := 0; i < len(input)-StartOfPacketMarkerLength; i++ {
 		marker := input[i : i+StartOfPacketMarkerLength]
 		if AllCharactersUnique(marker) {
 			return marker
@@ -15,6 +22,22 @@ func FindMarker(input string) string {
 	return ""
 }
 
+func GetMarkerIndex(signal string, marker string) int {
+	return strings.Index(signal, marker) + StartOfPacketMarkerLength
+}
+
 func AllCharactersUnique(input string) bool {
-	return false
+	unique := true
+	for i := 0; i < len(input); i++ {
+		s, other := string(input[i]), strings.Join([]string{input[:i], input[i+1:]}, "")
+		unique = unique && !strings.Contains(other, s)
+	}
+	return unique
+}
+
+func main() {
+	signal := utils.ReadFile("resources/input.txt")[0]
+	marker := FindMarker(signal)
+	index := GetMarkerIndex(signal, marker)
+	fmt.Println(index)
 }
--- 2022/day06/main_test.go
+++ 2022/day06/main_test.go
@@ -18,12 +18,75 @@ func TestDay06(t *testing.T) {
 				Input:          "abcd",
 				ExpectedOutput: true,
 			},
+			{
+				Input:          "mjqj",
+				ExpectedOutput: false,
+			},
+			{
+				Input:          "jpqm",
+				ExpectedOutput: true,
+			},
 		}
 
 		for _, testCase := range testCases {
 			actual := AllCharactersUnique(testCase.Input)
 			if actual != testCase.ExpectedOutput {
 				t.Fatalf("unique characters in %q, expected %t got %t", testCase.Input, testCase.ExpectedOutput, actual)
+			}
+		}
+	})
+
+	t.Run("find unique characters", func(t *testing.T) {
+		testCases := []struct {
+			Input    string
+			Expected string
+		}{
+			{
+				Input:    "mjqjpqmgbljsphdztnvjfqwrcgsmlb",
+				Expected: "jpqm",
+			},
+			{
+				Input:    "bvwbjplbgvbhsrlpgdmjqwftvncz",
+				Expected: "vwbj",
+			},
+			{
+				Input:    "nppdvjthqldpwncqszvftbrmjlhg",
+				Expected: "pdvj",
+			},
+		}
+
+		for _, testCase := range testCases {
+			actual := FindMarker(testCase.Input)
+			if testCase.Expected != actual {
+				t.Fatalf("expected %q got %q", testCase.Expected, actual)
+			}
+		}
+	})
+
+	t.Run("find marker index", func(t *testing.T) {
+		testCases := []struct {
+			Input    string
+			Expected int
+		}{
+			{
+				Input:    "mjqjpqmgbljsphdztnvjfqwrcgsmlb",
+				Expected: 7,
+			},
+			{
+				Input:    "bvwbjplbgvbhsrlpgdmjqwftvncz",
+				Expected: 5,
+			},
+			{
+				Input:    "nppdvjthqldpwncqszvftbrmjlhg",
+				Expected: 6,
+			},
+		}
+
+		for _, testCase := range testCases {
+			marker := FindMarker(testCase.Input)
+			index := GetMarkerIndex(testCase.Input, marker)
+			if testCase.Expected != index {
+				t.Fatalf("case %q: expected %d got %d", testCase.Input, testCase.Expected, index)
 			}
 		}
 	})
