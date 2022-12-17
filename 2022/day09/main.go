1a59a1865bfb15e0648b73e2985d7f0376447d14
Nathan Mertins <nwmertins@gmail.com>
2022-12-17 09:28:22 -0600

    finish up movement tests

--- 2022/day09/main.go
+++ 2022/day09/main.go
@@ -24,7 +24,72 @@ type Rope struct {
 	Tail [2]int
 }
 
-func (r *Rope) UpdateTail() {}
+func (r *Rope) UpdateTail() {
+	xHead := r.Head[0]
+	yHead := r.Head[1]
+
+	xTail := r.Tail[0]
+	yTail := r.Tail[1]
+
+	switch xDiff, yDiff := xHead-xTail, yHead-yTail; {
+	// if head is 2 spaces to the right,
+	// then tail should move right 1 space
+	case xDiff > 1 && yDiff == 0:
+		r.Tail[0]++
+	// if head is 2 spaces below,
+	// then tail should move down 1 space
+	case yDiff < -1 && xDiff == 0:
+		r.Tail[1]--
+	// if head is 2 spaces to the left,
+	// then tail should move left 1 space
+	case xDiff < -1 && yDiff == 0:
+		r.Tail[0]--
+	// if head is 2 spaces above,
+	// then tail should move up 1 space
+	case yDiff > 1 && xDiff == 0:
+		r.Tail[1]++
+	// if head is 2 spaces above and 1 space right,
+	// then tail should move diagonally
+	case xDiff > 0 && yDiff > 1:
+		r.Tail[0]++
+		r.Tail[1]++
+	// if head is 2 spaces right and 1 space above,
+	// then tail should move diagonally
+	case xDiff > 1 && yDiff > 0:
+		r.Tail[0]++
+		r.Tail[1]++
+	// if head is 2 spaces above and 1 space left,
+	// then tail should move diagonally
+	case xDiff < 0 && yDiff > 1:
+		r.Tail[0]--
+		r.Tail[1]++
+	// if head is 2 spaces left and 1 space above,
+	// then tail should move diagonally
+	case xDiff < -1 && yDiff > 0:
+		r.Tail[0]--
+		r.Tail[1]++
+	// if head is 2 spaces below and 1 space right,
+	// then tail should move diagonally
+	case xDiff > 0 && yDiff < -1:
+		r.Tail[0]++
+		r.Tail[1]--
+	// if head is 2 spaces right and 1 space below,
+	// then tail should move diagonally
+	case xDiff > 1 && yDiff < 0:
+		r.Tail[0]++
+		r.Tail[1]--
+	// if head is 2 spaces below and 1 space left,
+	// then tail should move diagonally
+	case xDiff < 0 && yDiff < -1:
+		r.Tail[0]--
+		r.Tail[1]--
+	// if head is 2 spaces left and 1 space below,
+	// then tail should move diagonally
+	case xDiff < -1 && yDiff < 0:
+		r.Tail[0]--
+		r.Tail[1]--
+	}
+}
 
 func ParseInput(input []string) []Motion {
 	motions := make([]Motion, len(input))
--- 2022/day09/main_test.go
+++ 2022/day09/main_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"reflect"
 	"testing"
 
 	"github.com/nmertins/advent-of-code/2022/utils"
@@ -24,19 +25,129 @@ func TestDay09(t *testing.T) {
 
 	t.Run("test rope movement", func(t *testing.T) {
 		testCases := []struct {
-			rope        Rope
 			description string
+			initial     Rope
+			expected    Rope
 		}{
-			{Rope{Head: [2]int{0, 0}, Tail: [2]int{0, 0}}, "head covers tail, no movement"},
-			{Rope{Head: [2]int{1, 0}, Tail: [2]int{0, 0}}, "head one space right of tail, no movement"},
+			// no movement
+			{
+				"head covers tail, no movement",
+				Rope{Head: [2]int{0, 0}, Tail: [2]int{0, 0}},
+				Rope{Head: [2]int{0, 0}, Tail: [2]int{0, 0}},
+			},
+			{
+				"head one space right of tail, no movement",
+				Rope{Head: [2]int{1, 0}, Tail: [2]int{0, 0}},
+				Rope{Head: [2]int{1, 0}, Tail: [2]int{0, 0}},
+			},
+			{
+				"head one space left of tail, no movement",
+				Rope{Head: [2]int{-1, 0}, Tail: [2]int{0, 0}},
+				Rope{Head: [2]int{-1, 0}, Tail: [2]int{0, 0}},
+			},
+			{
+				"head one space above tail, no movement",
+				Rope{Head: [2]int{0, 1}, Tail: [2]int{0, 0}},
+				Rope{Head: [2]int{0, 1}, Tail: [2]int{0, 0}},
+			},
+			{
+				"head one space below tail, no movement",
+				Rope{Head: [2]int{0, -1}, Tail: [2]int{0, 0}},
+				Rope{Head: [2]int{0, -1}, Tail: [2]int{0, 0}},
+			},
+			{
+				"head upper right diagonal from tail, no movement",
+				Rope{Head: [2]int{1, 1}, Tail: [2]int{0, 0}},
+				Rope{Head: [2]int{1, 1}, Tail: [2]int{0, 0}},
+			},
+			{
+				"head upper left diagonal from tail, no movement",
+				Rope{Head: [2]int{-1, 1}, Tail: [2]int{0, 0}},
+				Rope{Head: [2]int{-1, 1}, Tail: [2]int{0, 0}},
+			},
+			{
+				"head lower right diagonal from tail, no movement",
+				Rope{Head: [2]int{1, -1}, Tail: [2]int{0, 0}},
+				Rope{Head: [2]int{1, -1}, Tail: [2]int{0, 0}},
+			},
+			{
+				"head lower left diagonal from tail, no movement",
+				Rope{Head: [2]int{-1, -1}, Tail: [2]int{0, 0}},
+				Rope{Head: [2]int{-1, -1}, Tail: [2]int{0, 0}},
+			},
+			// linear movement
+			{
+				"head two spaces right of tail, tail moves right one space",
+				Rope{Head: [2]int{2, 0}, Tail: [2]int{0, 0}},
+				Rope{Head: [2]int{2, 0}, Tail: [2]int{1, 0}},
+			},
+			{
+				"head two spaces below tail, tail moves down one space",
+				Rope{Head: [2]int{0, -2}, Tail: [2]int{0, 0}},
+				Rope{Head: [2]int{0, -2}, Tail: [2]int{0, -1}},
+			},
+			{
+				"head two spaces left of tail, tail moves left one space",
+				Rope{Head: [2]int{-2, 0}, Tail: [2]int{0, 0}},
+				Rope{Head: [2]int{-2, 0}, Tail: [2]int{-1, 0}},
+			},
+			{
+				"head two spaces above tail, tail moves up one space",
+				Rope{Head: [2]int{0, 2}, Tail: [2]int{0, 0}},
+				Rope{Head: [2]int{0, 2}, Tail: [2]int{0, 1}},
+			},
+			// diagonal movement
+			// upper right
+			{
+				"head two spaces above and one space right of tail, tail moves diagonally",
+				Rope{Head: [2]int{1, 2}, Tail: [2]int{0, 0}},
+				Rope{Head: [2]int{1, 2}, Tail: [2]int{1, 1}},
+			},
+			{
+				"head two spaces right and one space above tail, tail moves diagonally",
+				Rope{Head: [2]int{2, 1}, Tail: [2]int{0, 0}},
+				Rope{Head: [2]int{2, 1}, Tail: [2]int{1, 1}},
+			},
+			// upper left
+			{
+				"head two spaces above and one space left of tail, tail moves diagonally",
+				Rope{Head: [2]int{-1, 2}, Tail: [2]int{0, 0}},
+				Rope{Head: [2]int{-1, 2}, Tail: [2]int{-1, 1}},
+			},
+			{
+				"head two spaces left and one space above tail, tail moves diagonally",
+				Rope{Head: [2]int{-2, 1}, Tail: [2]int{0, 0}},
+				Rope{Head: [2]int{-2, 1}, Tail: [2]int{-1, 1}},
+			},
+			// lower right
+			{
+				"head two spaces below and one space right of tail, tail moves diagonally",
+				Rope{Head: [2]int{1, -2}, Tail: [2]int{0, 0}},
+				Rope{Head: [2]int{1, -2}, Tail: [2]int{1, -1}},
+			},
+			{
+				"head two spaces right and one space below tail, tail moves diagonally",
+				Rope{Head: [2]int{2, -1}, Tail: [2]int{0, 0}},
+				Rope{Head: [2]int{2, -1}, Tail: [2]int{1, -1}},
+			},
+			// lower left
+			{
+				"head two spaces below and one space left of tail, tail moves diagonally",
+				Rope{Head: [2]int{-1, -2}, Tail: [2]int{0, 0}},
+				Rope{Head: [2]int{-1, -2}, Tail: [2]int{-1, -1}},
+			},
+			{
+				"head two spaces left and one space below tail, tail moves diagonally",
+				Rope{Head: [2]int{-2, -1}, Tail: [2]int{0, 0}},
+				Rope{Head: [2]int{-2, -1}, Tail: [2]int{-1, -1}},
+			},
 		}
 
 		for _, testCase := range testCases {
-			testCase.rope.UpdateTail()
-			if testCase.rope.Head != [2]int{0, 0} && testCase.rope.Tail != [2]int{0, 0} {
+			testCase.initial.UpdateTail()
+			if !reflect.DeepEqual(testCase.initial, testCase.expected) {
 				t.Errorf("Test case: %q\n", testCase.description)
-				t.Errorf("expected head at %v, got %v\n", [2]int{0, 0}, testCase.rope.Head)
-				t.Errorf("expected tail at %v, got %v\n", [2]int{0, 0}, testCase.rope.Tail)
+				t.Errorf("expected rope at %v, got %v\n", testCase.expected, testCase.initial)
 			}
 		}
 	})
